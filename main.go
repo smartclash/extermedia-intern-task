550c7e6f409f3aa44482a221c5c16fd47ccbea7b
Karan Sanjeev <yo@alphaman.me>
2021-05-26 16:43:17 +0530

    [WIP] Completed 90% of the task

--- main.go
+++ main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"fmt"
 	"github.com/jszwec/csvutil"
 	"io"
 	"log"
 	"os"
+	"strings"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	dictionaryChannel := make(chan []Translation)
-	wordsChannel := make(chan []string)
+	//wordsChannel := make(chan []string)
+	repeatedWordsChannel := make(chan []RepeatsCount)
+	translatedChannel := make(chan string)
+	repeatedCSVChannel := make(chan string)
 	translatedText := ""
 
-	go ReadWords(wordsChannel)
+	//go ReadWords(wordsChannel)
 	go ReadDictionary(dictionaryChannel)
 
 	file, err := os.Open("t8.shakespeare.txt")
@@ -36,9 +42,82 @@ func main() {
 		translatedText += string(buff[:totalRead])
 	}
 
-	fmt.Println(translatedText)
-	<-wordsChannel
-	<-dictionaryChannel
+	theDictionary := <-dictionaryChannel
+
+	go TranslateShakespeare(theDictionary, translatedText, translatedChannel)
+	go CountWordRepeats(theDictionary, translatedText, repeatedWordsChannel)
+	go WriteFile(<-translatedChannel, "t8.shakespeare.translated.txt")
+	go WriteRepeatsCSV(<-repeatedWordsChannel, repeatedCSVChannel)
+	go WriteFile(<-repeatedCSVChannel, "frequency.csv")
+
+	wg.Wait()
+}
+
+func TranslateShakespeare(dictionary []Translation, text string, c chan<- string) {
+	var dictionaryPair []string
+	lowerText := strings.ToLower(text)
+
+	for _, translation := range dictionary {
+		dictionaryPair = append(dictionaryPair, translation.English, translation.French)
+	}
+
+	text = strings.NewReplacer(dictionaryPair...).Replace(lowerText)
+	c <- text
+}
+
+func CountWordRepeats(words []Translation, text string, c chan<- []RepeatsCount) {
+	var repeats []RepeatsCount
+	lowerText := strings.ToLower(text)
+
+	for _, word := range words {
+		theCount := strings.Count(lowerText, word.English)
+		repeats = append(repeats, RepeatsCount{
+			English:        word.English,
+			French: 		word.French,
+			Repetitions: 	theCount,
+		})
+	}
+
+	c <- repeats
+}
+
+func WriteFile(text string, name string) {
+	wg.Add(1)
+
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatal("Couldn't create", name, "file")
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, text)
+	if err != nil {
+		log.Fatal("Couldn't write to", name, "file")
+	}
+
+	if err = file.Sync(); err != nil {
+		log.Fatal("Couldn't write to", name, "file")
+	}
+
+	wg.Done()
+}
+
+func WriteRepeatsCSV(repeats []RepeatsCount, c chan<- string) {
+	var csvData []Frequency
+	for _, repeat := range repeats {
+		csvData = append(csvData, Frequency{
+			English:   repeat.English,
+			French:    repeat.French,
+			Frequency: repeat.Repetitions,
+		})
+	}
+
+	buff, err := csvutil.Marshal(csvData)
+	if err != nil {
+		log.Fatal("Couldn't generate CSV fields")
+	}
+
+	c <- string(buff)
 }
 
 func ReadWords(c chan<- []string) {
--- types.go
+++ types.go
@@ -4,3 +4,15 @@ type Translation struct {
 	English string
 	French  string
 }
+
+type RepeatsCount struct {
+	English		string
+	French 		string
+	Repetitions int
+}
+
+type Frequency struct {
+	English 	string `csv:"English Word"`
+	French 		string `csv:"French Word,omitempty"`
+	Frequency 	int
+}
